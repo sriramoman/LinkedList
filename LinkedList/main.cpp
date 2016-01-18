6a35c939f25affff898d1d1d05772f27c42a5646
sriramoman <sriramoman@gmail.com>
2016-01-17 23:58:01 -0700

    Linked List implementation using class

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -7,9 +7,81 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+using namespace::std;
+
+struct LinkedList{
+    LinkedList* nextNode;
+    string data;
+};
+
+class singlylist{
+    private:
+    LinkedList* head;
+    LinkedList* tail;
+    int count;
+    public:
+    singlylist(){
+        head=NULL;
+        count=0;
+    }
+    int size(){
+        return count;
+    }
+    void insert(string data,int position){
+        if (head==NULL && position==0) {
+            LinkedList* newNode = (LinkedList*)malloc(sizeof(LinkedList));
+            newNode->data=data;
+            head=tail=newNode;
+            tail->nextNode=NULL;
+            ++count;
+            return;
+        }
+        if (position>count) {
+            cout<<"Insertion not possible"<<endl;
+            return;
+        }
+        else if (position==count){
+            //Save time in iterating
+            LinkedList* newNode = (LinkedList*)malloc(sizeof(LinkedList));
+            newNode->data=data;
+            tail->nextNode=newNode;
+            tail=newNode;
+            tail->nextNode=NULL;
+            ++count;
+        }
+        else{
+            LinkedList* newNode = (LinkedList*)malloc(sizeof(LinkedList));
+            newNode->data=data;
+            if (position==0) {
+                newNode->nextNode=head;
+                head=newNode;
+                return;
+            }
+            else {
+                LinkedList* iter=head;
+                for (int i=0; i<position; i++) {
+                    iter=iter->nextNode;
+                }
+                LinkedList* tmp=iter;
+                iter->nextNode=newNode;
+                newNode->nextNode=tmp->nextNode;
+            }
+        }
+    }
+    void display(int position);
+    void display();
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    singlylist objSinglyList;
+    objSinglyList.insert("World", 0);
+    objSinglyList.insert("Hello", 0);
+    objSinglyList.insert("Sriram", objSinglyList.size());
+    
+    singlylist objSinglyList1;
+    objSinglyList1.insert("Hello", 0);
+    std::cout << objSinglyList1.size()<<endl;
     return 0;
 }
